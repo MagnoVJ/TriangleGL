92fab5e07343fd00f4a524f24aa6b81573f0f91f
MagnoVJ <magnovj@gmail.com>
2024-07-03 18:57:47 -0300

    Creation of the Shader Class

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,9 @@
+#include "shader.hpp"
+
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 
 #include <iostream>
-
-#include "shader.hpp"
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 void process_input(GLFWwindow* window);
@@ -30,7 +30,33 @@ int main() {
 
 	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
 		std::cout << "Failed to Load GL Loader" << std::endl;
+		return -1;
 	}
+
+	Shader triangleShader("./src/sha_vertex.glsl", "./src/sha_fragment.glsl");
+
+	float vertices[] = {
+		-0.5f, -0.5f, 0.0f,
+		 0.5f, -0.5f, 0.0f,
+		 0.0f,  0.5f, 0.0f
+	};
+
+	unsigned int VBO, VAO;
+	glGenVertexArrays(1, &VAO);
+	glGenBuffers(1, &VBO);
+
+	glBindVertexArray(VAO);
+
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+	glEnableVertexAttribArray(0);
+
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+
+	glBindVertexArray(0);
 
 	while (!glfwWindowShouldClose(window)) {
 
@@ -38,6 +64,10 @@ int main() {
 
 		glClearColor(.2f, .3f, .3f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
+
+		triangleShader.use();
+		glBindVertexArray(VAO);
+		glDrawArrays(GL_TRIANGLES, 0, 3);
 
 		glfwSwapBuffers(window);
 		glfwPollEvents();
--- src/shader.hpp
+++ src/shader.hpp
@@ -12,15 +12,15 @@ class Shader {
 private:
 	void checkCompileErrors(unsigned int shader, std::string type);
 
-	void use();
-	void setBool(const std::string& name, bool value);
-	void setInt(const std::string& name, int value);
-	void setFloat(const std::string& name, float value);
-
 public:
 
 	unsigned int id;
 
 	Shader(const char* vertexPath, const char* fragmentPath);
+
+	void use();
+	void setBool(const std::string& name, bool value);
+	void setInt(const std::string& name, int value);
+	void setFloat(const std::string& name, float value);
 
 };
